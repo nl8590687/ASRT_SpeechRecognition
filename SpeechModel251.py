ee9f723f691f4721750dec9042dccb39f4348b7f
nl <3210346136@qq.com>
2019-01-06 22:04:12 +0800

    Modify default GPU configure

--- SpeechModel251.py
+++ SpeechModel251.py
@@ -396,8 +396,8 @@ if(__name__=='__main__'):
 	
 	#import tensorflow as tf
 	#from keras.backend.tensorflow_backend import set_session
-	#os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-	#进行配置，使用70%的GPU
+	#os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+	#进行配置，使用95%的GPU
 	#config = tf.ConfigProto()
 	#config.gpu_options.per_process_gpu_memory_fraction = 0.95
 	#config.gpu_options.allow_growth=True   #不全部占满显存, 按需分配
