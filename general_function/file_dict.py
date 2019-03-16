00502fd3b58078b777f18b382f5ed28e8923d19f
madeirak <haoran4@126.com>
2019-03-16 12:02:40 +0800

    fix file_dict.p
    解决dict.txt路径寻址

--- general_function/file_dict.py
+++ general_function/file_dict.py
@@ -3,16 +3,24 @@
 '''
 获取符号字典列表的程序
 '''
-
+import platform as plat
 
 def GetSymbolList(datapath):
 	'''
 	加载拼音符号列表，用于标记符号
 	返回一个列表list类型变量
 	'''
-	if(datapath != ''):
-		if(datapath[-1]!='/' or datapath[-1]!='\\'):
-			datapath = datapath + '/'
+
+	datapath_ = datapath.strip('dataset\\')
+
+	system_type = plat.system()  # 由于不同的系统的文件路径表示不一样，需要进行判断
+	if (system_type == 'Windows'):
+		datapath_ += '\\'
+	elif (system_type == 'Linux'):
+		datapath_ += '/'
+	else:
+		print('*[Message] Unknown System\n')
+		datapath_ += '/'
 	
 	txt_obj=open(datapath + 'dict.txt','r',encoding='UTF-8') # 打开文件并读入
 	txt_text=txt_obj.read()
@@ -26,4 +34,6 @@ def GetSymbolList(datapath):
 	list_symbol.append('_')
 	#SymbolNum = len(list_symbol)
 	return list_symbol
+
+
 	
