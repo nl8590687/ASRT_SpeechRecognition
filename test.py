6579229d7d4850a8c2d7b610eaae3e4e06103ac9
nl <3210346136@qq.com>
2019-03-29 14:28:01 +0800

    更新拼音参数和几条文件路径，声学模型文件跟之前版本不再兼容，需要重新训练

--- test.py
+++ test.py
@@ -8,7 +8,7 @@
 import platform as plat
 
 from SpeechModel251 import ModelSpeech
-from LanguageModel import ModelLanguage
+from LanguageModel2 import ModelLanguage
 from keras import backend as K
 
 datapath = ''
@@ -16,7 +16,7 @@ modelpath = 'model_speech'
 
 system_type = plat.system() # 由于不同的系统的文件路径表示不一样，需要进行判断
 if(system_type == 'Windows'):
-	datapath = 'E:\\语音数据集'
+	datapath = 'D:\\语音数据集'
 	modelpath = modelpath + '\\'
 elif(system_type == 'Linux'):
 	datapath = 'dataset'
@@ -29,14 +29,14 @@ else:
 ms = ModelSpeech(datapath)
 
 #ms.LoadModel(modelpath + 'm22_2\\0\\speech_model22_e_0_step_257000.model')
-ms.LoadModel(modelpath + 'm251\\speech_model251_e_0_step_117000.model')
+ms.LoadModel(modelpath + 'm251\\speech_model251_e_0_step_12000.model')
 
 #ms.TestModel(datapath, str_dataset='test', data_count = 64, out_report = True)
 r = ms.RecognizeSpeech_FromFile('D:\\语音数据集\\ST-CMDS-20170001_1-OS\\20170001P00241I0052.wav')
-#r = ms.RecognizeSpeech_FromFile('E:\语音数据集\ST-CMDS-20170001_1-OS\\20170001P00241I0053.wav')
-#r = ms.RecognizeSpeech_FromFile('E:\\语音数据集\\ST-CMDS-20170001_1-OS\\20170001P00020I0087.wav')
-#r = ms.RecognizeSpeech_FromFile('E:\\语音数据集\\wav\\train\\A11\\A11_167.WAV')
-#r = ms.RecognizeSpeech_FromFile('E:\\语音数据集\\wav\\test\\D4\\D4_750.wav')
+#r = ms.RecognizeSpeech_FromFile('D:\语音数据集\ST-CMDS-20170001_1-OS\\20170001P00241I0053.wav')
+#r = ms.RecognizeSpeech_FromFile('D:\\语音数据集\\ST-CMDS-20170001_1-OS\\20170001P00020I0087.wav')
+#r = ms.RecognizeSpeech_FromFile('D:\\语音数据集\\data_thchs30\\data\\A11_167.WAV')
+#r = ms.RecognizeSpeech_FromFile('D:\\语音数据集\\data_thchs30\\data\\D4_750.wav')
 
 K.clear_session()
 
