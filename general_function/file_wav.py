418c82637e6bc970f0ba60c1a6da24c473c30974
nl <3210346136@qq.com>
2019-04-04 12:38:14 +0800

    add raise value error for getfrequencyfeature function

--- general_function/file_wav.py
+++ general_function/file_wav.py
@@ -41,6 +41,9 @@ def GetMfccFeature(wavsignal, fs):
 	return wav_feature
 
 def GetFrequencyFeature(wavsignal, fs):
+	if(16000 != fs):
+		raise ValueError('[Error] ASRT currently only supports wav audio files with a sampling rate of 16000 Hz, but this audio is ' + str(fs) + ' Hz. ')
+	
 	# wav波形 加时间窗以及时移10ms
 	time_window = 25 # 单位ms
 	data_input = []
@@ -73,6 +76,9 @@ def GetFrequencyFeature(wavsignal, fs):
 	return data_input
 
 def GetFrequencyFeature2(wavsignal, fs):
+	if(16000 != fs):
+		raise ValueError('[Error] ASRT currently only supports wav audio files with a sampling rate of 16000 Hz, but this audio is ' + str(fs) + ' Hz. ')
+	
 	# wav波形 加时间窗以及时移10ms
 	time_window = 25 # 单位ms
 	window_length = fs / 1000 * time_window # 计算窗长度的公式，目前全部为400固定值
@@ -107,6 +113,9 @@ x=np.linspace(0, 400 - 1, 400, dtype = np.int64)
 w = 0.54 - 0.46 * np.cos(2 * np.pi * (x) / (400 - 1) ) # 汉明窗
 
 def GetFrequencyFeature3(wavsignal, fs):
+	if(16000 != fs):
+		raise ValueError('[Error] ASRT currently only supports wav audio files with a sampling rate of 16000 Hz, but this audio is ' + str(fs) + ' Hz. ')
+	
 	# wav波形 加时间窗以及时移10ms
 	time_window = 25 # 单位ms
 	window_length = fs / 1000 * time_window # 计算窗长度的公式，目前全部为400固定值
@@ -140,6 +149,9 @@ def GetFrequencyFeature4(wavsignal, fs):
 	'''
 	主要是用来修正3版的bug
 	'''
+	if(16000 != fs):
+		raise ValueError('[Error] ASRT currently only supports wav audio files with a sampling rate of 16000 Hz, but this audio is ' + str(fs) + ' Hz. ')
+	
 	# wav波形 加时间窗以及时移10ms
 	time_window = 25 # 单位ms
 	window_length = fs / 1000 * time_window # 计算窗长度的公式，目前全部为400固定值
@@ -149,8 +161,8 @@ def GetFrequencyFeature4(wavsignal, fs):
 	wav_length = wav_arr.shape[1]
 	
 	range0_end = int(len(wavsignal[0])/fs*1000 - time_window) // 10 + 1 # 计算循环终止的位置，也就是最终生成的窗数
-	data_input = np.zeros((range0_end, 200), dtype = np.float) # 用于存放最终的频率特征数据
-	data_line = np.zeros((1, 400), dtype = np.float)
+	data_input = np.zeros((range0_end, window_length // 2), dtype = np.float) # 用于存放最终的频率特征数据
+	data_line = np.zeros((1, window_length), dtype = np.float)
 	
 	for i in range(0, range0_end):
 		p_start = i * 160
@@ -163,7 +175,7 @@ def GetFrequencyFeature4(wavsignal, fs):
 		data_line = np.abs(fft(data_line)) / wav_length
 		
 		
-		data_input[i]=data_line[0:200] # 设置为400除以2的值（即200）是取一半数据，因为是对称的
+		data_input[i]=data_line[0: window_length // 2] # 设置为400除以2的值（即200）是取一半数据，因为是对称的
 		
 	#print(data_input.shape)
 	data_input = np.log(data_input + 1)
