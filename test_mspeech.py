7b9630ac05adde9cc39963ef97d29786de8de4ac
PhanatosZou <tongzou@uw.edu>
2020-03-26 14:04:53 -0700

    compatible with TF2.0

--- test_mspeech.py
+++ test_mspeech.py
@@ -17,10 +17,12 @@ from SpeechModel251 import ModelSpeech
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 #进行配置，使用90%的GPU
-config = tf.ConfigProto()
+config = tf.compat.v1.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = 0.9
 #config.gpu_options.allow_growth=True   #不全部占满显存, 按需分配
-set_session(tf.Session(config=config))
+sess = tf.compat.v1.Session(config=config)
+tf.compat.v1.keras.backend.set_session(sess)
+
 
 
 datapath = ''
