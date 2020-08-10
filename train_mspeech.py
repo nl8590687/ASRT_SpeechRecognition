87a0ca090e0670891f719c0754a59d9f8ab1f8e6
nl <3210346136@qq.com>
2020-08-10 16:35:40 +0800

    自动创建保存模型的子目录

--- train_mspeech.py
+++ train_mspeech.py
@@ -12,7 +12,7 @@ import tensorflow as tf
 from keras.backend.tensorflow_backend import set_session
 
 
-from SpeechModel251 import ModelSpeech
+from SpeechModel251 import ModelSpeech, ModelName
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 #进行配置，使用95%的GPU
@@ -29,6 +29,7 @@ modelpath = 'model_speech'
 
 if(not os.path.exists(modelpath)): # 判断保存模型的目录是否存在
 	os.makedirs(modelpath) # 如果不存在，就新建一个，避免之后保存模型的时候炸掉
+	os.makedirs(modelpath + '/m' + ModelName)
 
 system_type = plat.system() # 由于不同的系统的文件路径表示不一样，需要进行判断
 if(system_type == 'Windows'):
