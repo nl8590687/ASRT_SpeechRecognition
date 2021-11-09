b97511573aee1bc354db30d68df4db8cb168805d
nl <3210346136@qq.com>
2021-11-09 21:33:29 +0800

    feat: 一些基础功能模块的实现

---
+++ utils/config.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2016-2099 Ailemon.net
+#
+# This file is part of ASRT Speech Recognition Tool.
+#
+# ASRT is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# ASRT is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ASRT.  If not, see <https://www.gnu.org/licenses/>.
+# ============================================================================
+
+"""
+@author: nl8590687
+加载ASRT配置文件相关
+"""
+
+import json
+
+default_config_filename = 'asrtConfig.json'
+_config_dict = None
+_pinyin_dict = None
+_pinyin_list = None
+
+def load_config_file(filename) -> dict:
+    '''
+    加载json配置文件
+
+    参数：\\
+        filename: 文件名
+
+    返回：\\
+        配置信息字典
+    '''
+    global _config_dict
+    if _config_dict != None:
+        return _config_dict
+
+    with open(filename,'r', encoding="utf-8") as fp:
+        _config_dict = json.load(fp)
+    return _config_dict
+
+def load_pinyin_dict(filename) -> tuple:
+    '''
+    加载拼音列表和拼音字典
+
+    拼音列表：用于下标索引转拼音 \\
+    拼音字典：用于拼音索引转下标
+    '''
+    global _pinyin_list, _pinyin_dict
+    if _pinyin_dict!=None and _pinyin_list!=None:
+        return _pinyin_list, _pinyin_dict
+    
+    _pinyin_list = list()
+    _pinyin_dict = dict()
+    with open(filename, 'r', encoding='utf-8') as fp:
+        lines = fp.read().split('\n')
+    for line in lines:
+        if len(line) == 0:
+            continue
+        tokens = line.split('\t')
+        _pinyin_list.append(tokens[0])
+        _pinyin_dict[tokens[0]] = len(_pinyin_list) - 1
+    return _pinyin_list, _pinyin_dict
---
+++ utils/ops.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2016-2099 Ailemon.net
+#
+# This file is part of ASRT Speech Recognition Tool.
+#
+# ASRT is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# ASRT is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ASRT.  If not, see <https://www.gnu.org/licenses/>.
+# ============================================================================
+
+"""
+@author: nl8590687
+一些常用操作函数的定义
+"""
+
+import wave
+import numpy as np
+import difflib
+
+def read_wav_data(filename: str) -> tuple:
+    '''
+    读取一个wav文件，返回声音信号的时域谱矩阵和播放时间
+    '''
+    wav = wave.open(filename,"rb") # 打开一个wav格式的声音文件流
+    num_frame = wav.getnframes() # 获取帧数
+    num_channel=wav.getnchannels() # 获取声道数
+    framerate=wav.getframerate() # 获取帧速率
+    num_sample_width=wav.getsampwidth() # 获取实例的比特宽度，即每一帧的字节数
+    str_data = wav.readframes(num_frame) # 读取全部的帧
+    wav.close() # 关闭流
+    wave_data = np.fromstring(str_data, dtype = np.short) # 将声音文件数据转换为数组矩阵形式
+    wave_data.shape = -1, num_channel # 按照声道数将数组整形，单声道时候是一列数组，双声道时候是两列的矩阵
+    wave_data = wave_data.T # 将矩阵转置
+    return wave_data, framerate, num_channel, num_sample_width
+
+
+def get_edit_distance(str1, str2) -> int:
+	leven_cost = 0
+	s = difflib.SequenceMatcher(None, str1, str2)
+	for tag, i1, i2, j1, j2 in s.get_opcodes():
+		#print('{:7} a[{}: {}] --> b[{}: {}] {} --> {}'.format(tag, i1, i2, j1, j2, str1[i1: i2], str2[j1: j2]))
+		if tag == 'replace':
+			leven_cost += max(i2-i1, j2-j1)
+		elif tag == 'insert':
+			leven_cost += (j2-j1)
+		elif tag == 'delete':
+			leven_cost += (i2-i1)
+	return leven_cost
