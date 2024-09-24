9375ecb2db6f0cafd359f2b27cfa373927d63e4c
nl8590687 <3210346136@qq.com>
2024-09-25 02:23:38 +0800

    规范代码

--- model_zoo/speech_model/pytorch_backend.py
+++ model_zoo/speech_model/pytorch_backend.py
@@ -1,53 +1,79 @@
-import torch
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2016-2099 Ailemon.net
+#
+# This file is part of ASRT Speech Recognition Tool.
+#
+# ASRT is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# ASRT is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ASRT.  If not, see <https://www.gnu.org/licenses/>.
+# ============================================================================
+
+"""
+@author: nl8590687 / Evelynn-n
+若干 pytorch版声学模型模型的定义和实现
+"""
+
 import torch.nn as nn
-import torch.nn.functional as F
+import torch.nn.functional as function
 import tqdm
+
+
 class SpeechModel251BN(nn.Module):
-    def __init__(self,input_shape: tuple = (1600, 200, 1), output_size: int = 1428):
+    def __init__(self, input_shape: tuple = (1600, 200, 1), output_size: int = 1428):
         super(SpeechModel251BN, self).__init__()
 
         self.input_shape = input_shape
         self._pool_size = 8
         self._model_name = 'SpeechModel251bn'
         self.output_shape = (input_shape[0] // self._pool_size, output_size)
-        
-        self.conv0 = nn.Conv2d(1, 32, kernel_size=(3, 3),padding='same')
-        self.bn0 = nn.BatchNorm2d(32,eps=0.0002)
 
-        self.conv1 = nn.Conv2d(32, 32, kernel_size=(3, 3),padding='same')
-        self.bn1 = nn.BatchNorm2d(32,eps=0.0002)
-        
+        self.conv0 = nn.Conv2d(1, 32, kernel_size=(3, 3), padding='same')
+        self.bn0 = nn.BatchNorm2d(32, eps=0.0002)
+
+        self.conv1 = nn.Conv2d(32, 32, kernel_size=(3, 3), padding='same')
+        self.bn1 = nn.BatchNorm2d(32, eps=0.0002)
+
         # self.maxpool1 = F.max_pool2d(kernel_size=(2, 2), stride=(2, 2))
 
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=(3, 3),padding='same')
-        self.bn2 = nn.BatchNorm2d(64,eps=0.0002)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=(3, 3), padding='same')
+        self.bn2 = nn.BatchNorm2d(64, eps=0.0002)
 
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=(3, 3),padding='same')
-        self.bn3 = nn.BatchNorm2d(64,eps=0.0002)
-        
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=(3, 3), padding='same')
+        self.bn3 = nn.BatchNorm2d(64, eps=0.0002)
+
         # self.maxpool2 = F.max_pool2d(kernel_size=(2, 2), stride=(2, 2))
 
-        self.conv4 = nn.Conv2d(64, 128, kernel_size=(3, 3),padding='same')
-        self.bn4 = nn.BatchNorm2d(128,eps=0.0002)
+        self.conv4 = nn.Conv2d(64, 128, kernel_size=(3, 3), padding='same')
+        self.bn4 = nn.BatchNorm2d(128, eps=0.0002)
 
-        self.conv5 = nn.Conv2d(128, 128, kernel_size=(3, 3),padding='same')
-        self.bn5 = nn.BatchNorm2d(128,eps=0.0002)
-        
+        self.conv5 = nn.Conv2d(128, 128, kernel_size=(3, 3), padding='same')
+        self.bn5 = nn.BatchNorm2d(128, eps=0.0002)
+
         # self.maxpool3 = F.max_pool2d(kernel_size=(2, 2), stride=(2, 2))
 
-        self.conv6 = nn.Conv2d(128, 128, kernel_size=(3, 3),padding='same')
-        self.bn6 = nn.BatchNorm2d(128,eps=0.0002)
+        self.conv6 = nn.Conv2d(128, 128, kernel_size=(3, 3), padding='same')
+        self.bn6 = nn.BatchNorm2d(128, eps=0.0002)
 
-        self.conv7 = nn.Conv2d(128, 128, kernel_size=(3, 3),padding='same')
-        self.bn7 = nn.BatchNorm2d(128,eps=0.0002)
+        self.conv7 = nn.Conv2d(128, 128, kernel_size=(3, 3), padding='same')
+        self.bn7 = nn.BatchNorm2d(128, eps=0.0002)
 
         # self.maxpool4 = F.max_pool2d(kernel_size=(1,1),stride=(1,1))
 
-        self.conv8 = nn.Conv2d(128, 128, kernel_size=(3, 3),padding='same')
-        self.bn8 = nn.BatchNorm2d(128,eps=0.0002)
+        self.conv8 = nn.Conv2d(128, 128, kernel_size=(3, 3), padding='same')
+        self.bn8 = nn.BatchNorm2d(128, eps=0.0002)
 
-        self.conv9 = nn.Conv2d(128, 128, kernel_size=(3, 3),padding='same')
-        self.bn9 = nn.BatchNorm2d(128,eps=0.0002)
+        self.conv9 = nn.Conv2d(128, 128, kernel_size=(3, 3), padding='same')
+        self.bn9 = nn.BatchNorm2d(128, eps=0.0002)
 
         # self.maxpool5 = F.max_pool2d(kernel_size=(1,1),stride=(1,1))
 
@@ -58,41 +84,43 @@ class SpeechModel251BN(nn.Module):
         self.ctc_loss = nn.CTCLoss(blank=0)
 
     def forward(self, x):
-        x = F.relu(self.bn0(self.conv0(x)))
-        x = F.relu(self.bn1(self.conv1(x)))
+        x = function.relu(self.bn0(self.conv0(x)))
+        x = function.relu(self.bn1(self.conv1(x)))
         # print(x.size())
-        x = F.max_pool2d(x, kernel_size=(2, 2), stride=(2, 2))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
+        x = function.max_pool2d(x, kernel_size=(2, 2), stride=(2, 2))
+        x = function.relu(self.bn2(self.conv2(x)))
+        x = function.relu(self.bn3(self.conv3(x)))
         # print(x.size())
-        x = F.max_pool2d(x, kernel_size=(2, 2), stride=(2, 2))
-        x = F.relu(self.bn4(self.conv4(x)))
-        x = F.relu(self.bn5(self.conv5(x)))
+        x = function.max_pool2d(x, kernel_size=(2, 2), stride=(2, 2))
+        x = function.relu(self.bn4(self.conv4(x)))
+        x = function.relu(self.bn5(self.conv5(x)))
         # print(x.size())
-        x = F.max_pool2d(x, kernel_size=(2, 2), stride=(2, 2))
-        x = F.relu(self.bn6(self.conv6(x)))
-        x = F.relu(self.bn7(self.conv7(x)))
+        x = function.max_pool2d(x, kernel_size=(2, 2), stride=(2, 2))
+        x = function.relu(self.bn6(self.conv6(x)))
+        x = function.relu(self.bn7(self.conv7(x)))
         # print(x.size())
-        x = F.max_pool2d(x, kernel_size=(1, 1), stride=(1, 1))
-        x = F.relu(self.bn8(self.conv8(x)))
-        x = F.relu(self.bn9(self.conv9(x)))
+        x = function.max_pool2d(x, kernel_size=(1, 1), stride=(1, 1))
+        x = function.relu(self.bn8(self.conv8(x)))
+        x = function.relu(self.bn9(self.conv9(x)))
         # print(x.size())
-        x = F.max_pool2d(x, kernel_size=(1, 1), stride=(1, 1))
+        x = function.max_pool2d(x, kernel_size=(1, 1), stride=(1, 1))
         # print(x.size())
         x = x.permute(0, 2, 3, 1)
-        x = x.reshape(x.size(0),x.size(1), -1)
+        x = x.reshape(x.size(0), x.size(1), -1)
         # x = x.permute(0, 2, 1)
         # x = x.view(x.size(0),x.size(1), -1)
         # print(x.size())
-        x = F.relu(self.dense0(x))
+        x = function.relu(self.dense0(x))
         # print(x.size())
-        x = F.softmax(self.dense1(x))
+        x = function.softmax(self.dense1(x))
         # print(x.size())
         return x
-    def compute_loss(self, y_pred,labels,input_length,label_length):
+
+    def compute_loss(self, y_pred, labels, input_length, label_length):
         y_pred = y_pred.permute(1, 0, 2)
         loss = self.ctc_loss(y_pred, labels, input_length, label_length)
         return loss
+
     def train_model(self, train_loader, optimizer, num_epochs=10, device='cpu'):
         self.to(device)
         self.train()
@@ -118,28 +146,8 @@ class SpeechModel251BN(nn.Module):
             avg_loss = epoch_loss / len(train_loader)
             print(f"Epoch {epoch + 1}/{num_epochs}, Loss: {avg_loss:.4f}")
 
-        # for epoch in range(num_epochs):
-        #     epoch_loss = 0.0
-
-        #     for batch in train_loader:
-        #         inputs, labels, input_lengths, label_lengths = batch
-        #         inputs = inputs.to(device)
-        #         labels = labels.to(device)
-        #         input_lengths = input_lengths.to(device)
-        #         label_lengths = label_lengths.to(device)
-
-        #         optimizer.zero_grad()
-        #         y_pred = self.forward(inputs)
-
-        #         loss = self.compute_loss(y_pred, labels, input_lengths, label_lengths)
-        #         loss.backward()
-        #         optimizer.step()
-
-        #         epoch_loss += loss.item()
-
-        #     avg_loss = epoch_loss / len(train_loader)
-        #     print(f"Epoch {epoch + 1}/{num_epochs}, Loss: {avg_loss:.4f}")
     def get_model(self):
         return self
+
     def get_model_name(self) -> str:
         return self._model_name
--- torch_speech_model.py
+++ torch_speech_model.py
@@ -1,8 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2016-2099 Ailemon.net
+#
+# This file is part of ASRT Speech Recognition Tool.
+#
+# ASRT is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# ASRT is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ASRT.  If not, see <https://www.gnu.org/licenses/>.
+# ============================================================================
+
+"""
+@author: nl8590687 / Evelynn-n
+声学模型基础功能模板定义
+"""
+
 import os
 import torch
 from torch.utils.data import Dataset, DataLoader
+
+
 class SpeechDataset(Dataset):
-    def __init__(self, data_loader,speech_features, input_shape, max_label_length,device='cpu'):
+    def __init__(self, data_loader, speech_features, input_shape, max_label_length, device='cpu'):
         self.data_loader = data_loader
         self.input_shape = input_shape
         self.speech_features = speech_features
@@ -14,10 +41,10 @@ class SpeechDataset(Dataset):
         return self.data_count
     
     def __getitem__(self, index):
-        wavdata, sample_rate, data_labels = self.data_loader.get_data(index)
-        
+        wav_data, sample_rate, data_labels = self.data_loader.get_data(index)
+
         # 提取特征
-        data_input = self.speech_features.run(wavdata, sample_rate)
+        data_input = self.speech_features.run(wav_data, sample_rate)
         data_input = data_input.reshape(data_input.shape[0], data_input.shape[1], 1)
 
         # 计算输入长度，确保不超出最大序列长度
@@ -25,9 +52,9 @@ class SpeechDataset(Dataset):
         inlen = min(data_input.shape[0] // pool_size + data_input.shape[0] % pool_size, self.input_shape[0] // 8)
 
         # 初始化输入特征数组，填充到 `input_shape` 大小
-        X = torch.zeros(self.input_shape)
-        X[:len(data_input)] = torch.tensor(data_input,dtype=torch.float32)
-        X = X.permute(2, 0, 1)
+        x = torch.zeros(self.input_shape)
+        x[:len(data_input)] = torch.tensor(data_input, dtype=torch.float32)
+        x = x.permute(2, 0, 1)
 
         # 初始化标签数组，填充到 `max_label_length` 大小
         y = torch.zeros(self.max_label_length, dtype=torch.int16)
@@ -36,38 +63,40 @@ class SpeechDataset(Dataset):
         # 转换为 PyTorch 张量
         input_length = torch.tensor([inlen], dtype=torch.float32)
         label_length = torch.tensor([len(data_labels)], dtype=torch.float32)
-        return X, y, input_length, label_length
+        return x, y, input_length, label_length
+
+
 class ModelSpeech:
     def __init__(self, speech_model, speech_features, max_label_length=64):
         """模型初始化"""
         self.speech_model = speech_model
-        self.trained_model= speech_model.get_model()
+        self.trained_model = speech_model.get_model()
         self.speech_features = speech_features
         self.max_label_length = max_label_length
-    def train(self,data_loader,epochs,batch_size,optimizer,save_step=1,last_epoch=0,device='cpu'):
+
+    def train(self, data_loader, epochs, batch_size, optimizer, save_step=1, last_epoch=0, device='cpu'):
         """训练模型"""
-        save_filename = os.path.join('save_models_torch',self.speech_model.get_model_name() + '.pth')
+        save_filename = os.path.join('save_models_torch', self.speech_model.get_model_name() + '.pth')
         self.trained_model.to(device)
         print('[ASRT] torch model successfully initialized to device: {}'.format(device))
         data_loader = DataLoader(data_loader, batch_size=batch_size, shuffle=True)
         model = self.speech_model
         for epoch in range(epochs):
-            print('[ASRT] Epoch {}/{}'.format(epoch+1,epochs))
+            print('[ASRT] Epoch {}/{}'.format(epoch+1, epochs))
             epoch_loss = 0.0
             for batch in data_loader:
-                X, y, input_length, label_length = batch
-                X = X.to(device)
+                x, y, input_length, label_length = batch
+                x = x.to(device)
                 y = y.to(device)
                 input_length = input_length.to(device).unsqueeze(1).long()
                 label_length = label_length.to(device).unsqueeze(1).long()
                 optimizer.zero_grad()
-                y_pred = model(X)
+                y_pred = model(x)
                 # print(y_pred.shape, y.shape, input_length.shape, label_length.shape)
                 loss = model.compute_loss(y_pred, y, input_length, label_length)
                 loss.backward()
                 optimizer.step()
                 epoch_loss += loss.item()
-            avg_loss = epoch_loss / len(data_loader)
-            print(f"Epoch {epoch + 1}/{epochs}, Loss: {avg_loss:.4f}")
 
-
+                avg_loss = epoch_loss / len(data_loader)
+                print(f"Epoch {epoch + 1}/{epochs}, Loss: {avg_loss:.4f}")
